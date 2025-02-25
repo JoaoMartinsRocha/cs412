ed5282e9b7893e82f6a44890a69b35455b5c9ce7
JoaoMartinsRocha <jprocha@bu.edu>
2025-02-25 09:40:17 -0500

    Added feature that displays status messages on profile pages

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from.models import Article
+from .forms import CreateArticleForm, CreateCommentForm
 import random
+from django.urls import reverse
 # Create your views here.
 
 class ShowAllView(ListView):
@@ -30,3 +32,60 @@ class RandomArticleView(DetailView):
         all_articles = Article.objects.all()
         article = random.choice(all_articles)
         return article
+    
+
+class CreateArticleView(CreateView):
+    '''A view to handle creation of a new Article.
+    (1) display the HTML form to user (GET)
+    (2) process the form submission and store the new Article object (POST)
+    '''
+
+    form_class = CreateArticleForm
+    template_name = "blog/create_article_form.html"
+
+class CreateCommentView(CreateView):
+    '''A view to create a new comment on an Article and save it to the database.'''
+
+    form_class = CreateCommentForm
+    template_name = "blog/create_comment_form.html"
+    
+    def get_success_url(self):
+        '''Return the URL to redirect to after successfully submitting form.'''
+        # return reverse('show_all')
+        ## note: this is not ideal, because we are redirected to the main page.
+        pk = self.kwargs['pk']
+        return reverse('article', kwargs={'pk':pk})
+    
+    def get_context_data(self):
+        '''Return the dictionary of context variables for use in the template.'''
+
+        # calling the superclass method
+        context = super().get_context_data()
+
+        # find/add the article to the context data
+        # retrieve the PK from the URL pattern
+        pk = self.kwargs['pk']
+        article = Article.objects.get(pk=pk)
+
+        # add this article into the context dictionary:
+        context['article'] = article
+        return context
+
+    def form_valid(self, form):
+        '''This method handles the form submission and saves the 
+        new object to the Django database.
+        We need to add the foreign key (of the Article) to the Comment
+        object before saving it to the database.
+        '''
+        
+		# instrument our code to display form fields: 
+        print(f"CreateCommentView.form_valid: form.cleaned_data={form.cleaned_data}")
+        
+        # retrieve the PK from the URL pattern
+        pk = self.kwargs['pk']
+        article = Article.objects.get(pk=pk)
+        # attach this article to the comment
+        form.instance.article = article # set the FK
+
+        # delegate the work to the superclass method form_valid:
+        return super().form_valid(form)
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -20,3 +20,24 @@ class Profile(models.Model):
     def __str__(self):
         '''Return a string representation of this Article object.'''
         return f'{self.first_name} {self.last_name}\'s profile'
+    
+    def get_all_status_messages(self):
+        '''Return all of the comments about this article.'''
+        comments = StatusMessage.objects.filter(profile=self)
+        return comments
+    
+
+class StatusMessage(models.Model):
+    '''Encapsulate the idea of an Article by some author.'''
+
+    # data attributes of the StatusMessage model:
+
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    message = models.TextField(blank=False)
+    timestamp = models.DateTimeField(auto_now=True)
+    
+    
+    
+    def __str__(self):
+        '''Return a string representation of this Article object.'''
+        return f'{self.message}'
